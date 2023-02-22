d6523add17e2bf435c2d144bf3496ba62ca81aac
berhtz <zuteho@gmail.com>
2023-02-22 10:40:27 +0500

    project structure changed

---
+++ cmd/auth/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/Berhtz/go-auth/dbconn"
+	"github.com/Berhtz/go-auth/handlers"
+	"github.com/Berhtz/go-auth/models"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	// load .env variables
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file in auth/cmd/main.go")
+	}
+
+	//migrations
+	db := dbconn.DbConnect()
+	dbHasTable := db.Migrator().HasTable(&models.User{})
+	if dbHasTable == false {
+		db.Migrator().CreateTable(&models.User{})
+	}
+
+	http.HandleFunc("/api/signup", handlers.SignUp)
+	http.HandleFunc("/api/signin", handlers.SignIn)
+
+	log.Fatal(http.ListenAndServe(":8000", nil))
+}
